abc19462fe145438c7e8638781248b65d5d792e2
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-03-27 10:32:08 +0000

    Printf Project Task 0 and 1: Attempt 1

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _puts(char *s);
+int _print_integer(int n);
+int _printf(const char *format, ...);
+
+#endif
